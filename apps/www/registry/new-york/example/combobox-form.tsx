a2a2ba38b192a25327b7cc99794170e001ef00d5
Anurag <anurajput62616@gmail.com>
2024-06-30 17:03:57 +0000

    fix: combobox

--- apps/www/registry/new-york/example/combobox-form.tsx
+++ apps/www/registry/new-york/example/combobox-form.tsx
@@ -75,38 +75,34 @@ export default function ComboboxForm() {
               <FormLabel>Language</FormLabel>
               <Popover>
                 <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn(
-                        "w-[200px] justify-between",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value
-                        ? languages.find(
-                            (language) => language.value === field.value
-                          )?.label
-                        : "Select language"}
-                      <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </FormControl>
+                  <Button
+                    variant="outline"
+                    role="combobox"
+                    className="w-[200px] justify-between"
+                  >
+                    {field.value
+                      ? languages.find(
+                          (language) => language.value === field.value
+                        )?.label
+                      : "Select language"}
+                    <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                  </Button>
                 </PopoverTrigger>
                 <PopoverContent className="w-[200px] p-0">
                   <Command>
                     <CommandInput
-                      placeholder="Search framework..."
+                      placeholder="Search language..."
                       className="h-9"
                     />
-                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandEmpty>No language found.</CommandEmpty>
                     <CommandGroup>
                       {languages.map((language) => (
                         <CommandItem
                           value={language.label}
                           key={language.value}
                           onSelect={() => {
-                            form.setValue("language", language.value)
+                            console.log(`Language selected: ${language.value}`);
+                            form.setValue("language", language.value);
                           }}
                         >
                           {language.label}
